44c1f91485fb2a7d4837c355ce2d51c5feebb6a0
Lucas Farah <lucasfarah@Lucass-MacBook-Pro.local>
2015-04-08 00:10:38 -0700

    pota

--- server.py
+++ server.py
@@ -90,10 +90,8 @@ def remov():
 	return "Last item removed"
 
 
-if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+	app.run()
 
 
 
