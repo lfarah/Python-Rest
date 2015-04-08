314d905c3209c2f8a46c7c96115ac50e71a0ff75
Lucas Farah <lucasfarah@Lucass-MacBook-Pro.local>
2015-04-07 23:49:02 -0700

    pota

--- server.py
+++ server.py
@@ -1,71 +1,100 @@
-#!/usr/bin/env python
-import os
-import web
-import xml.etree.ElementTree as ET
-import numbers
+from flask import Flask,request,jsonify
+import json
 
-tree = ET.parse('user_data.xml')
-root = tree.getroot()
+app = Flask(__name__)
 
-urls = (
-	'/users', 'list_users',
-	'/potato/(.*)', 'get_user',
-	'/write', 'write_users',
-	'/getXml', 'get_xml',
-	'/', 'greetings'
+@app.route('/hello')
+def api_hello():
+	if 'name' in request.args:
+		list = [
+		{'name': request.args['name'], 'age': request.args['age']},
+		]
+	# jsonify will do for us all the work, returning the
+	# previous data structure in JSON
+		return jsonify(results=list)
+
+	else:
+		return 'Hello John Doe'
+
+@app.route("/")
+def welcome():
+	return "\n" + "Welcome to the website. Possible method: http://127.0.0.1:5000/hello?name=Lucas&age=30"
+
+@app.route("/save")
+def wc():
+	my_dict = {                   
+  'Name':      'KAIRA',
+  'Location':  u'Kilpisj\u00E4rvi',
+  'Longitude': 20.76,
+  'Latitude':  69.07
+	}
+	my_dict2 = {                   
+  'Name':      'KAIRA',
+  'Location':  u'Kilpisj\u00E4rvi',
+  'Longitude': 20.76,
+  'Latitude':  69.07
+	}
+	myArray = [my_dict,my_dict2]
+	out_file = open("test.json","w")
+	json.dump(myArray,out_file, indent=4)                                    
+	out_file.close()
+	return "saved"
+
+@app.route("/open")
+def opn():
+	# Open the file for reading
+	in_file = open("test.json","r")
+
+# Load the contents from the file, which creates a new dictionary
+	array = json.load(in_file)
+
+# Close the file... we don't need it anymore  
+	in_file.close()
+
+# Print the contents of our freshly loaded dictionary
+	return jsonify(results=array)
+
+@app.route("/add")
+def addNew():
+	# Open the file for reading
+	inFiled = open("test.json","r")
+	arr = json.load(inFiled)
+	inFiled.close()
+
+	my_dict2 = {                   
+  'Name':      'KURIO',
+  'Location':  u'Kilpisj\u00E4rvi',
+  'Longitude': 20.76,
+  'Latitude':  69.07
+	}
+
+	arr.append(my_dict2)
+# Load the contents from the file, which creates a new array
+
+	
+	out_file = open("test.json","w")
+	json.dump(arr,out_file, indent=4)                                    
+	out_file.close()
+	return "added"
+
+@app.route("/remove")
+def remov():
+	in_file = open("test.json","r")
+	array = json.load(in_file)
+	in_file.close()
+	array.pop()
+	
+	out_file = open("test.json","w")
+	json.dump(array,out_file, indent=4)                                    
+	out_file.close()
+	return "Last item removed"
 
 
-)
-
-app = web.application(urls, globals())
-
-class greetings:
-	def GET(self):
-		return "Welcome!"
-
-class get_xml:
-	def GET(self):
-		answer = ""
-		for node in root:
-			answer += str(node.attrib)
-		return answer
-
-
-class write_users:
-	def GET(self):
-		doc = ET.SubElement(root, "user")
-		co  = ET.SubElement(doc, 'age', name='15')
-	 	ppl = ET.SubElement(co, 'name', name='Luis Lopez')
-	 	idd  = ET.SubElement(doc, 'id', name='30')
-
-		tree = ET.ElementTree(root)
-		doc.append(co)
-		doc.append(ppl)
-		doc.append(idd)
-
-		root.append(doc)
-		tree.write("user_data.xml")
-		return "wrote new line"
-
-class list_users:        
-	def GET(self):
-		output = 'users:[';
-		for child in root:
-			print 'child', child.tag, child.attrib
-			output += str(child.attrib) + ','
-		output += ']';
-		return output
-
-class get_user:
-	def GET(self, user):
-		answer = ""
-		if user.isdigit():
-			answer = str(int(user)*3)
-
-		else:
-			answer = user + " potato"
-		return answer
-
-if __name__ == '__main__':
-	_port = int(os.environ.get('PORT', 8080))
+if __name__ == "__main__":
 	app.run()
+
+
+
+
+
+   #return 'Hello ' + request.args['name'] + ', ' + request.args['age']
