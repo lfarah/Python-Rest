41faa3572df8f9c3f5c2a7096f53ad7747969426
Lucas Farah <lucasfarah@Lucass-MacBook-Pro.local>
2015-04-08 00:24:41 -0700

    pota

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import json
 import os
 
 app = Flask(__name__)
+
 @app.route('/hello')
 def api_hello():
 	if 'name' in request.args:
@@ -91,8 +92,8 @@ def remov():
 
 
 if __name__ == "__main__":
-	app.run()
-
+	port = int(os.environ.get("PORT", 5000))
+	app.run(debug=True, host='0.0.0.0', port=port)
 
 
 
